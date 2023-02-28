9c3f6d659417dd2243be1b80fa972444f6322bcf
Yajusenpai <1051148569@qq.com>
2023-02-28 15:00:14 -0500

    revised design, add/remove still needs work

--- Cache/CacheRouter.py
+++ Cache/CacheRouter.py
@@ -1,21 +1,21 @@
 # Consistent Hashing Routing
 import hashlib as hl
-from collections import OrderedDict
+from Node import Node
+
 
 class Router:
     def __int__(self, nodes: int):
         self.total_nodes = nodes
-        self.current_nodes = nodes
-        self.address_book = []    # add IP addresses here
-        self.node_status = {}  # show whether each node is active
+        self.current_nodes_num = nodes
+        self.current_nodes = []
 
     def route(self, key: str) -> int:
-        hashed = hl.md5().update(bytes(key))
-        return int(hashed.hexdigest(), 16) % self.current_nodes
+        hashed = hl.md5()
+        hashed.update(bytes(key.encode('utf-8')))
+        return int(hashed.hexdigest(), 16) % self.current_nodes_num
 
-    def add_node(self, ip_address: str) -> None:
-        self.address_book.append(ip_address)
-        self.node_status.update({ip_address: True})
+    def add_node(self, ip_address: str, name: str) -> None:
+        self.current_nodes.append(Node.__int__(name, ip_address, status=True))
 
-    def remove_node(self, ip_address: str) -> None:
-        self.node_status.update({ip_address: False})
+    def remove_node(self, ip_address: str, name: str) -> None:
+        self.current_nodes.remove(Node.__int__(name, ip_address, status=True))
---
+++ Cache/Node.py
@@ -0,0 +1,5 @@
+class Node:
+    def __int__(self, name: str, address: str, status: bool):
+        self.name = name
+        self.address = address
+        self.status = status
