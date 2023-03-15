ae21d9b6164f918089f507037f708be129da2204
GloriaWa <h536wang@uwaterloo.ca>
2023-03-15 07:55:56 -0400

    UI

--- user_app/main.py
+++ user_app/main.py
@@ -152,7 +152,7 @@ def cache_pool_change():
 
 @webapp.route('/pool_change', methods=['GET', 'POST'])
 def pool_change_page():
-    render_template("pool_change.html")
+    return render_template("pool_change.html")
 
 # ________________________auto test api _________________________
 # @webapp.route('/api/delete_all', methods=['POST'])
